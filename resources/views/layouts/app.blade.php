70702da25f1c872aec8b226b2d6168c4f41901c4
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-09-08 15:42:59 -0500

    Product Setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <h1>@yield('title')</h1>
+        <nav>
+            <ul>
+                <li><a href="#"">products</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/product/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', $viewData['title'])
+@section('content')
+<div>
+</div>
+@stop
