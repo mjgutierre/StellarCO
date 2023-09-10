af4f4608ce9e707dc010daeba227f964110c6ad6
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-09-10 12:56:05 -0500

    Add images and fix db types

--- database/migrations/2023_09_10_174802_create_reviews_table.php
+++ database/migrations/2023_09_10_174802_create_reviews_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products');
             $table->timestamps();
--- resources/views/test/admin/product/create.blade.php
+++ resources/views/test/admin/product/create.blade.php
@@ -1,9 +1,11 @@
-<form method="POST" action="{{ route('admin.product.save') }}">
+<form method="POST" action="{{ route('admin.product.save') }}" enctype="multipart/form-data">
   @csrf
   <input type="text" class="form-control mb-2" placeholder="Enter name" name="name" />
   <input type="text" class="form-control mb-2" placeholder="Enter description" name="description" />
   <input type="number" class="form-control mb-2" placeholder="Enter price" name="price" />
   <input type="number" class="form-control mb-2" placeholder="Enter quantity" name="quantity" />
   <input type="text" class="form-control mb-2" placeholder="Enter location" name="location" />
+  <input class="form-control" type="file" name="image">
+  &nbsp;
   <input type="submit" class="btn btn-primary" value="Send" />
 </form>
