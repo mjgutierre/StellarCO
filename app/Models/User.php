e965866306e99ae9dd2059401b5c9e758bccad7e
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-09-10 16:35:18 -0500

    Modify user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,20 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
+      * USER ATTRIBUTES
+      * $this->attributes['id'] - int - contains the user primary key (id) 
+      * $this->attributes['name'] - string - contains the user name
+      * $this->attributes['email'] - string - contains the user email
+      * $this->attributes['email_verified_at'] - timestamp - contains the user email verification date 
+      * $this->attributes['password'] - string - contains the user password
+      * $this->attributes['remember_token'] - string - contains the user password
+      * $this->attributes['role'] - string - contains the user role (customer or admin)
+      * $this->attributes['balance'] - int - contains the user balance
+      * $this->attributes['created_at'] - timestamp - contains the user creation date
+      * $this->attributes['updated_at'] - timestamp - contains the user update date
+    */
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
@@ -21,6 +35,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'balance'
     ];
 
     /**
@@ -42,4 +57,86 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    //GETTERS
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
+
+    public function getEmailVerifiedAt(): string
+    {
+        return $this->attributes['email_verified_at'];
+    }
+
+    public function getPassword(): string
+    {
+        return $this->attributes['password'];
+    }
+
+    public function getRememberToken(): string
+    {
+        return $this->attributes['remember_token'];
+    }
+
+    public function getRole(): string
+    {
+        return $this->attributes['role'];
+    }
+
+    public function getBalance(): int
+    {
+        return $this->attributes['balance'];
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    //SETTERS
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    public function setEmailVerifiedAt(string $email_verified_at): void
+    {
+        $this->attributes['email_verified_at'] = $email_verified_at;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->attributes['password'] = $password;
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->attributes['role'] = $role;
+    }
+
+    public function setBalance(int $balance): void
+    {
+        $this->attributes['balance'] = $balance;
+    }
 }
