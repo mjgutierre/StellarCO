6d6ecf351b0813b61b63532bf52dc71367df7cb8
Sebastian Arias Usma <sebasarias33314@gmail.com>
2023-09-11 23:21:59 -0500

    Product Filters Created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -25,4 +25,36 @@ class ProductController extends Controller
 
     return view('product.show')->with('viewData', $viewData);
   }
+
+  public function getProductsOrderedAsc(): View
+  {
+    $viewData = [];
+    $viewData['title'] = 'Products Ordered by Quantity';
+    $viewData['products'] = Product::orderBy('quantity', 'asc')->get();
+    return view('product.ordered-asc')->with('viewData', $viewData);
+  }
+
+  public function getProductsOrderedDsc(): View
+  {
+    $viewData = [];
+    $viewData['title'] = 'Products Ordered by Quantity';
+    $viewData['products'] = Product::orderBy('quantity', 'desc')->get();
+    return view('product.ordered-dsc')->with('viewData', $viewData);
+  }
+
+  public function getProductsOrderedNameAsc(): View
+  {
+    $viewData = [];
+    $viewData['title'] = 'Products Ordered by Quantity';
+    $viewData['products'] = Product::orderBy('name', 'asc')->get();
+    return view('product.ordered-name-asc')->with('viewData', $viewData);
+  }
+
+  public function getProductsOrderedNameDsc(): View
+  {
+    $viewData = [];
+    $viewData['title'] = 'Products Ordered by Quantity';
+    $viewData['products'] = Product::orderBy('name', 'desc')->get();
+    return view('product.ordered-name-dsc')->with('viewData', $viewData);
+  }
 }
