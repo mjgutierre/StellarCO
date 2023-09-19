b9b9d7814d13dd825fe401a0395c04f9ebbac0dd
Sebastian Arias Usma <83093117+sebasarias17@users.noreply.github.com>
2023-09-19 17:41:39 -0500

    Sa statistics (#21)
    * Statistics Created
    
    * Customer quantity added
    
    * Files fixed
    
    * User quantity added

--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -5,5 +5,6 @@
     <p class="analytics">Se han creado {{$viewData['rockets']}} cohetes.</p>
     <p class="analytics">Tenemos  ${{$viewData['countries']}} de dólares en cohetes.</p>
     <p class="analytics">Los clientes suelen tener {{$viewData['rocketsAvg']}} cohetes.</p>
+    <p class="analytics">Tenemos {{$viewData['customerquant']}} usuarios de tipo Customer </p>
 </div>
 @endsection
