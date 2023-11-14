d5df5838aa99a864378afa489784d2a7d55a90cc
unknown <mjgutierre@eafit.edu.co>
2023-11-14 16:33:28 -0500

    vista de perfil

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,29 +1,36 @@
 @extends('layouts.app')
+
 @section('title', $viewData['title'])
+
 @section('content')
 <div class="container user-profile-container">
     <div class="row">
-        <div class="col-md-6 offset-md-3">
-                <h2>Perfil de Usuario</h2>
-                <div class="card">
-                    <div class="card-body">
-                        <form>
-                            <div class="mb-3">
-                                <label for="name" class="form-label">Nombre</label>
-                                <input type="text" class="form-control" id="name" value="{{ $viewData['user']->getName() }}" readonly>
-                            </div>
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Correo Electrónico</label>
-                                <input type="email" class="form-control" id="email" value="{{ $viewData['user']->getEmail() }}" readonly>
-                            </div>
-                            <div class="mb-3">
-                                <label for="balance" class="form-label">Saldo</label>
-                                <input type="text" class="form-control" id="balance" value="${{ number_format($viewData['user']->getBalance(), 2) }}" readonly>
-                            </div>
-                        </form>
+        <div class="col-md-3 offset-md-3">
+            <div class="user-profile-card card">
+                <div class="user-profile-header card-header">
+                    <h2 class="user-profile-title">Perfil de Usuario</h2>
+                </div>
+                <div class="user-profile-body card-body">
+                    <div class="user-profile-info">
+                        <div class="user-profile-item">
+                            <i class="fas fa-user user-profile-icon"></i>
+                            <label for="name" class="form-label">Nombre</label>
+                            <div class="user-profile-value">{{ $viewData['user']->getName() }}</div>
+                        </div>
+                        <div class="user-profile-item">
+                            <i class="fas fa-envelope user-profile-icon"></i>
+                            <label for="email" class="form-label">Correo Electrónico</label>
+                            <div class="user-profile-value">{{ $viewData['user']->getEmail() }}</div>
+                        </div>
+                        <div class="user-profile-item">
+                            <i class="fas fa-wallet user-profile-icon"></i>
+                            <label for="balance" class="form-label">Saldo</label>
+                            <div type="text" class="user-profile-value">${{ number_format($viewData['user']->getBalance(), 2) }}</div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
